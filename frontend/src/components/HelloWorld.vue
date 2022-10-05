182c5351937d596ad236900e3c26a6a792f93e01
Georgii <georgii.gubarkov@codeblock78.ru>
2022-10-05 16:48:18 +0300

    feat: init frontend

---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,58 @@
+<template>
+  <v-form>
+    <v-container>
+      <v-row>
+        <v-col
+          cols="12"
+          md="4"
+        >
+          <v-text-field
+            :counter="10"
+            label="Login"
+            required
+          ></v-text-field>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col
+        cols="12"
+        md="4"
+      >
+        <v-text-field
+          :counter="10"
+          label="Password"
+          required
+        ></v-text-field>
+      </v-col>
+      </v-row>
+      <v-row>
+        <v-col
+        cols="12"
+        md="4"
+      >
+      <v-btn
+      :disabled="!valid"
+      color="success"
+      class="mr-4"
+      @click="validate"
+    >
+      Login
+    </v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-form>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      valid: true,
+    }),
+    methods: {
+      validate() {
+        console.log("Login")
+      },
+    },
+  }
+</script>
---
+++ frontend/src/main.js
@@ -0,0 +1,17 @@
+import 'vuetify/styles' // Global CSS has to be imported
+import { createApp } from 'vue'
+import { createVuetify } from 'vuetify'
+import App from './App.vue'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const app = createApp(App)
+const vuetify = createVuetify({
+  components,
+  directives,
+  ssr: true,
+}) // Replaces new Vuetify()
+
+app.use(vuetify)
+
+app.mount('#app')
