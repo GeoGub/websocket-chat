98ef94908cac4b73a06f20d8af70264bf2dccadc
Georgii <georgii.gubarkov@codeblock78.ru>
2022-10-07 19:08:53 +0300

    feat: message response return user_schema with message schema

--- app/src/crud/crud_message.py
+++ app/src/crud/crud_message.py
@@ -1,12 +1,22 @@
 from databases import Database
+from sqlalchemy import select, func
 
 from .crud_base import CRUDBase
 from src.message.model import message
+from src.user.model import user
+from src.sample_schemas import Params
 
 
 class CRUDMessage(CRUDBase):
-    pass
-    # async def read(self, db: Database):
-    #     pass
+    async def read(self, db: Database, params: Params):
+        query = select(self.model.join(user)).limit(params.limit).offset(params.limit*params.offset)        
+        if params.dir == "asc":
+            query = query.order_by(self.model.c.id.asc())
+        else:
+            query = query.order_by(self.model.c.id.desc())
+        messages = await db.fetch_all(query)
+        total = await db.execute(select(func.count()).select_from(self.model))
+        return messages, total
+        
 
 crud_message = CRUDMessage(message)
